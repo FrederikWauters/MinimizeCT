1782caa3530871e833f76fd78a9bd8266a9d64ab
FrederikWauters <fwauters@uw.edu>
2014-12-11 11:22:39 -0800

    got it workin

--- Analyzer.h
+++ Analyzer.h
@@ -8,6 +8,9 @@
 #include <fstream>
 
 #include "Math/Minimizer.h"
+#include "Math/Functor.h"
+#include "TH2.h"
+#include "TGraph.h"
 
 class DataManager;
 
@@ -17,22 +20,31 @@ class Analyzer {
 
   public:
 
-    Analyzer(int dof , DataManager* datamanager,const char* minName, const char* algoName );
+    Analyzer(int dof, DataManager* datamanager,const char* minName, const char* algoName );
     virtual ~Analyzer();
 
-    int InitMinimizer( int dof,const char * minName, const char *algoName);
-    int Run();
+    int InitMinimizer(int dof, const char * minName, const char *algoName);
+    int Run(double low1, double low2, double high1, double high2, double step);
+    int TestRun(double x, double y, int dof); //Get predicted values
 
     double ChiSqrFunction2(const double *xx);
     double ChiSqrFunction4(const double *xx);
+    double GetChiSqr(double ca, double cs, double csp, double ct, double ctp, double vud);
+
+
 
   private:
  
 
     ROOT::Math::Minimizer* minimizer;
-    std::vector<TData*>* data;
+    ROOT::Math::Functor functor;
+    std::vector<TData*> data;
     DataManager* manager;
 
+    double par1, par2; //The coupling constants to loop over. Either cs and csp, or (ct+ctp)/ca and (ct-ctp)/ca 
+    double cv, ca_SM;
+
+    
 
 
 };
--- DataManager.h
+++ DataManager.h
@@ -6,8 +6,15 @@
 #include <iostream>
 #include <cstring>
 #include <fstream>
+#include <TGFrame.h>
+
 
 #include "TFile.h"
+
+#include "TH1.h"
+#include "TH2.h"
+#include "TGraph.h"
+#include "TMultiGraph.h"
 
 
 
@@ -22,16 +29,44 @@ class DataManager {
     int SetOutputFile(char* outfile);
 
     TFile *fout;
+    int dof; //determines which mode we're running in
+
+    TH2F* hCa;
+    TH2F* hVud;
+    TH2F* hChiSqr;
+    TH2F* hPDF;
+    
+    TH1F* hPDF_X;
+    TH1F* hPDF_Y;
+    
+    TGraph* g68CL;
+    TGraph* g90CL;
+    TGraph* g95CL;
+    TGraph* gScale;
+    TMultiGraph* mga;
 
   public:
     
-    DataManager(char* fin, char* fout);
+    DataManager(char* fin, char* fout, int d);
     virtual ~DataManager();
 
     std::vector<TData*> data;
     void PrintData();
-    std::vector<TData*>* GetData() {return &data;};
+    std::vector<TData*> GetData() {return data;}
     
+    void SetOutput(double min, const double *xs,double par1, double par2);
+
+    double PDFValue(double x,int n);
+    void InitHistos(double low1, double low2, double high1, double high2, double step);
+    void Plot();
+    void WriteOutput();
+
+    //post processing
+    void ConstructContour(TH2* h1, TH2* h2, TGraph* g);
+    void MakeCLContours(int nPoints, double dChiSqr);
+    void Make1DContours();
+
+
 
     
 };
